f7608f5f2360280e96075485447fb6ce01a8c01d
66eli77 <66eli77@gmail.com>
2016-12-01 22:43:42 -0800

    restore to 4.9

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ export ROOTDIR=$(dirname $(readlink -f $0))
 export HOSTPYTHON=$ROOTDIR/hostpython
 export HOSTPGEN=$ROOTDIR/hostpgen
 
-export NDK="$HOME/Desktop/android-ndk-r7c" #change this to point to your NDK folder
+export NDK="$HOME/Desktop/android-ndk-r13b" #change this to point to your NDK folder
 export SDK="$HOME/Desktop/android-sdk-linux/" #change this to point to your SDK folder
-export NDKPLATFORM="$NDK/platforms/android-14/arch-arm"
+export NDKPLATFORM="$NDK/platforms/android-24/arch-arm"
 
-export PATH="$NDK/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86_64/bin/:$NDK:$SDK/tools:$PATH"
+export PATH="$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/:$NDK:$SDK/tools:$PATH"
 
 export PYVERSION="2.7.2"
 
